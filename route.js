dea3f7372dd444203b11ab7cb7e5c859327c430d
Vira <mazvira@ukr.net>
2020-04-02 19:32:51 +0300

    Fix sending data

--- route.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const data_controller = require('./data_controller.js');
-
-module.exports = function (app) {
-    app.route("/data")
-        .get(data_controller.getData);
-};
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-//let bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express(),
     port = process.env.PORT || 8080;
 
-//app.use(bodyParser.urlencoded({extended: false}))
 app.use(session({
     key: 'user_sid',
     secret: 'Shh, its a secret!',
@@ -16,11 +14,9 @@ app.use(session({
     }
 }));
 
-const routes = require("./route.js");
-
 app.route("/data")
     .get((req, res) => {
-        res.sendFile(__dirname + '/index.html');
+        res.sendFile(__dirname + '/stat.csv');
     });
 
 
@@ -28,7 +24,5 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-
-routes(app);
 
 app.listen(port, () => console.log("server running at localhost:" + port));
